2f0552ac0606a251d0683dfbfd6ee9fad8769fa6
Olivia Thet <olivia@turnkey.io>
2024-03-14 13:04:55 -0400

    Update to v1.1.1

--- Formula/turnkey.rb
+++ Formula/turnkey.rb
@@ -1,20 +1,20 @@
 class Turnkey < Formula
   desc "Turnkey CLI"
   homepage "https://github.com/tkhq/tkcli"
-  version "v1.0.5"
+  version "v1.1.1"
   license "Apache License 2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/tkhq/tkcli/raw/v1.0.5/dist/turnkey.darwin-aarch64", using: CurlDownloadStrategy
-    sha256 "03ed1ce9fbe2ae91f3f78fea1051459130b917746c854fb7856604a605587acf"
+    url "https://github.com/tkhq/tkcli/raw/v1.1.1/dist/turnkey.darwin-aarch64", using: CurlDownloadStrategy
+    sha256 "e57e8abdaf069d8fc7574472140cf593d8a5ab20fe7e8cab2966daff2d1a54d7"
 
     def install
       bin.install "turnkey.darwin-aarch64" => "turnkey"
     end
   end
   if Hardware::CPU.intel?
-    url "https://github.com/tkhq/tkcli/raw/v1.0.5/dist/turnkey.darwin-x86_64", using: CurlDownloadStrategy
-    sha256 "a9721fc5dc7edd5de60740a60113d8a7195392db5f81c0240898ab2c9cb46fd7"
+    url "https://github.com/tkhq/tkcli/raw/v1.1.1/dist/turnkey.darwin-x86_64", using: CurlDownloadStrategy
+    sha256 "bc644a9f0123a425982ff2e790b46151a9f2c5a0523e82d8a7d5ce26c547596b"
 
     def install
       bin.install "turnkey.darwin-x86_64" => "turnkey"
